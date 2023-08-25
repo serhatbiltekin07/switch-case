e220a29b3e30bd5719f4a098be848ecbcbcca971
BİLTEKİN KURTULUŞ <112376639+serhatbiltekin07@users.noreply.github.com>
2023-08-25 15:01:53 +0300

    Add files via upload

---
+++ Switch-Case-Yapısı/Switch-Case-Yapısı/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case_Yapısı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month) 
+            {
+                case 6:
+                    Console.WriteLine("Haziran ayındasınız!");
+                    break;
+             case 7:
+                 Console.WriteLine("Temmuz ayındasınız!");
+                    break;
+             case 8:
+                    Console.WriteLine("Ağustos ayındasınız!");
+                    break;
+
+                case 9:
+                    Console.WriteLine("Eylül ayındasınız!");
+                    break;
+
+                default:
+                    Console.WriteLine("Yanlış Veri girişi");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız!");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız!");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız!");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Son bahar ayındasınız!");
+                    break;
+
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
